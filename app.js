b08f9b79c075f84b8a011cce1de0cc0bb4aa66ad
Hitesh Sharma <hiteshsharmahs0987@gmail.com>
2022-12-24 12:28:20 +0530

    aa

--- app.js
+++ app.js
@@ -27,9 +27,10 @@ app.use(require('./routes/user'))
 
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
     const path = require('path')
-    app.get("*",(req,res)=>{
+
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build')))
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
